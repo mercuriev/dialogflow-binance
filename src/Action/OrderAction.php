f6e7de8d91783efc64774483f4708526a96bdf19
Max Mercuriev <max@yogi.pw>
2021-04-03 13:41:02 +0700

    save new orders to db

--- src/Action/OrderAction.php
+++ src/Action/OrderAction.php
@@ -4,6 +4,8 @@ namespace Action;
 use Hook\Request;
 use Hook\Response;
 use Larislackers\BinanceApi\Exception\BinanceApiException;
+use Db\TableGateway;
+use Db\Exception\RecordExists;
 
 final class OrderAction extends AbstractAction
 {
@@ -27,18 +29,28 @@ final class OrderAction extends AbstractAction
         ];
 
         try {
-            $resp = $this->api->postOrder($params);
+            $order = $this->api->postOrder($params);
+            $order = json_decode($order->getBody(), true);
+            if (!$order) throw new \RuntimeException($order->getBody());
         } catch (BinanceApiException $e) {
             $this->log->err($e->getMessage());
             $res->addText($e->getMessage());
             return $res;
         }
 
-        $resp = json_decode($resp->getBody(), true);
-        if (!$resp) throw new \RuntimeException($resp->getBody());
+        $orders = new TableGateway('order', $this->db);
+        try {
+            $orders->insert([
+                'id' => $order['orderId'],
+                'symbol' => $order['symbol'],
+                'raw'   => json_encode($order)
+            ]);
+        } catch (RecordExists $e) {
+            $this->log->warn($e->getMessage());
+        }
 
         $res->addText(vsprintf('%s %s %u: %.7g for %.7g', [
-            $resp['side'], $symbol, $resp['orderId'], $resp['origQty'], $resp['price'],
+            $order['side'], $symbol, $order['orderId'], $order['origQty'], $order['price'],
         ]));
 
         return $res;
